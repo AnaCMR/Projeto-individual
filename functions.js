fdf077dbd184c5239d130ed0a88bb316b93f15d3
Ana Marques Rodrigues <ana.silva@bandtec.com.br>
2021-10-31 01:16:06 -0300

    Finalização tela hortoterapia e urban-jungle

--- functions.js
+++ functions.js
@@ -2,3 +2,17 @@ function subscribe(){
 
 return false;
 }
+
+function like() {
+    var numeros = numbers_like.innerHTML
+    numbers_like.innerHTML = Number(numeros)+1;
+    change_like.style.display = "block"
+    like_heart.style.display = "none"
+}
+
+function deslike(){
+    var numeros = numbers_like.innerHTML
+    numbers_like.innerHTML = Number(numeros)-1;
+    change_like.style.display = "none"
+    like_heart.style.display = "block"
+}
