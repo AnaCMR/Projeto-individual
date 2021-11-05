f34f655cf0c2c5e1e8e3bff6a6a3157db1fe591c
AnaCMR <ana.silva@bandtec.com.br>
2021-11-05 12:45:34 -0300

    Continuação da criação da tela de perfil

---
+++ api-site/perfil.js
@@ -0,0 +1,17 @@
+function change_email() {
+    show_email.style.display = "block"
+    show_password.style.display = "none"
+}
+
+function confirm_email(){
+    show_email.style.display = "none"
+}
+
+function change_password() {
+    show_email.style.display = "none"
+    show_password.style.display = "block"
+}
+
+function confirm_password(){
+    show_password.style.display = "none"   
+}
